b2b0ed265cb40ee60e9c54eee840b17cb365b92d
meko3 <hirono.yoneyama@outlook.com>
2019-01-08 18:34:18 +0900

    [feat] 2. Introducing JSX

--- app/javascript/packs/hello_world.tsx
+++ app/javascript/packs/hello_world.tsx
@@ -1,9 +1,47 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+interface User {
+  firstname: string;
+  lastname: string;
+  avatarUrl: string;
+}
+
+function getGreeting(username?: User) {
+  if (username) {
+    return <h1>Hello, {formatName(username)}!<div>{ele2}</div></h1>
+  }
+  return <h1>Hello, Stranger.<div>{ele1}</div></h1>
+}
+
+function formatName(username: User) {
+  return username.firstname + " " + username.lastname;
+};
+
+const user = {
+  firstname: "Harper",
+  lastname: "Perez",
+  avatarUrl: "http://www.google.com",
+};
+
+const ele1 = <div tabIndex={1}>good</div>;
+const ele2 = <img src={user.avatarUrl} />;
+
+const element = getGreeting();
+
+const object = React.createElement(
+  "h1",
+  {className: "greeting"},
+  "Hello, World",
+);
+
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <h1>Hello, world</h1>,
+    element,
+    document.body.appendChild(document.createElement("div")),
+  );
+  ReactDOM.render(
+    object,
     document.body.appendChild(document.createElement("div")),
   );
 });
--- docs/react-step-by-step.md
+++ docs/react-step-by-step.md
@@ -3,3 +3,18 @@
   - add imports (ReactDOM React)
   - document.getElementById() -> document.body.appendChild(document.createElement("div"))
 - 実際にアプリを作って進めるチュートリアルとコンセプトを理解するためのチュートリアルがあって良い
+- Reactを使うにあたって必要な知識は[Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript)
+  - 30分から１時間かかるx
+  - 3つの要点
+    - o let const  x var
+    - class
+    - allow function x => x * 2
+
+2. Introducing JSX
+- jsxではstringでもHTMLでもないelementが使われる
+- jsではロジックとレンダリング部分でソースファイルが異なるがこれはコンポーネントを表現するのに適していない
+- jsxを使う必要はないが使いやすさと見やすさで広く使われてる
+- セミコロンはつけようね
+- compileしたらjsになるのでifとかforも使えるよ
+- DOMがレンダリングする前にエスケープするのでXSSにつよい
+- ES6とJSXコードの両方が適切に表示されるようにBabelの言語定義を使うのを勧める
